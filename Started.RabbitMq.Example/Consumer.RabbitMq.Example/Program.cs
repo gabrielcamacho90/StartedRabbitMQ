2305287992c906854533d2280a0e4fc50b330429
Gabriel Camacho <gabriel.camacho@smlbrasil.com.br>
2021-05-16 20:16:35 -0300

    Inicio Repo

---
+++ Started.RabbitMq.Example/Consumer.RabbitMq.Example/Program.cs
@@ -0,0 +1,47 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace Consumer.RabbitMq.Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory factory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                Port = Protocols.DefaultProtocol.DefaultPort,
+                UserName = "admin",
+                Password = "admin",
+                VirtualHost = "/",
+                RequestedHeartbeat = TimeSpan.FromSeconds(60)
+            };
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    Console.WriteLine("Aguardando Logs");
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body.ToArray();
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine($" [x] Recebido: {message}");
+                    };
+
+                    channel.BasicConsume("TesteQueue", true, consumer);
+
+                    Console.WriteLine("Pressione [Enter] para sair!");
+                    Console.ReadLine();
+                    channel.Close();
+                }
+
+                connection.Close();
+            }
+        }
+    }
+}
---
+++ Started.RabbitMq.Example/Started.RabbitMq.Example/Program.cs
@@ -0,0 +1,47 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Started.RabbitMq.Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory factory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                Port = Protocols.DefaultProtocol.DefaultPort,
+                UserName = "admin",
+                Password = "admin",
+                VirtualHost = "/",
+                RequestedHeartbeat = TimeSpan.FromSeconds(60)
+            };
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.ExchangeDeclare("TesteExchange", ExchangeType.Topic, false);
+
+                    channel.QueueDeclare("TesteQueue", true, false, false, null);
+
+                    channel.QueueBind("TesteQueue", "TesteExchange", "TesteBind");
+
+                    var message = "Olá Teste RabbitMQ";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    var properties = channel.CreateBasicProperties();
+                    properties.Persistent = true;
+
+                    channel.BasicPublish("TesteExchange", "TesteBind", properties, body);
+
+                    Console.WriteLine($" [x] sent {message}");
+                }
+
+                Console.WriteLine("Pressione [Enter] para sair!");
+                Console.ReadLine();
+            }
+        }
+    }
+}
